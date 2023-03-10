d95aa8e9640c0b0a1f52fe0f998de5ccd2366aab
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-03-10 10:11:48 +0100

    Added a line for displaying the initial value of the item monitored

--- FirstWebApp/Controllers/OpcControllers/OpcUAReadController.cs
+++ FirstWebApp/Controllers/OpcControllers/OpcUAReadController.cs
@@ -1,18 +1,18 @@
 ﻿using System.Web.Mvc;
 using Opc.UaFx.Client;
 
-namespace FirstWebApp.Controllers
+namespace FirstWebApp.Controllers.OpcControllers
 {
     public class OpcUaReadController : Controller
     {
         private const string OpcUrl = "opc.tcp://127.0.0.1:4840";
 
-        public static string ReadNodeValue()
+        public static string ReadNodeValue(string nodeId)
         {
             var client = new OpcClient(OpcUrl);
             client.Connect();
 
-            var returnValue = client.ReadNode("ns=6;s=::Program:Cube.Status.StateCurrent");
+            var returnValue = client.ReadNode(nodeId);
             return returnValue.ToString();
         }
 
